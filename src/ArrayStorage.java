88553067ff0e125634e11c9b8d8cb69fc2a7e51f
cuidadoso <alexander_s@front.ru>
2017-07-09 12:33:15 +0300

    HW01 - add field size.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,20 +3,23 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        for(int i = 0; i < size(); i++)
+        for(int i = 0; i < size; i++)
         {
             storage[i] = null;
         }
+        calcSize();
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        calcSize();
     }
 
     Resume get(String uuid) {
-        for(int i = 0; i < size(); i++)
+        for(int i = 0; i < size; i++)
         {
             if(uuid.equals(storage[i].uuid))
                 return storage[i];
@@ -26,7 +29,7 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int delPos = -1;
-        for(int i = 0; i < size(); i++)
+        for(int i = 0; i < size; i++)
         {
             if(uuid.equals(storage[i].uuid))
             {
@@ -34,29 +37,32 @@ public class ArrayStorage {
                 break;
             }
         }
-        for(int i = delPos; i < size(); i++)
+        for(int i = delPos; i < size; i++)
         {
             storage[i] = storage[i + 1];
         }
+        calcSize();
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] result = new Resume[size()];
+        Resume[] result = new Resume[size];
         System.arraycopy(storage, 0, result, 0, size());
         return result;
     }
 
     int size() {
-        int size = 0;
+        return size;
+    }
+
+    void calcSize() {
         for(int i = 0; i < storage.length; i++)
         {
             size = i;
             if(storage[i] == null)
                 break;
         }
-        return size;
     }
 }
