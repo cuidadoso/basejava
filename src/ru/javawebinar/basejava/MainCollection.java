2bb7a2d93459aa394e345231c171b04593a09c45
cuidadoso <alexander_s@front.ru>
2017-07-12 22:06:58 +0300

    Lesson05

---
+++ src/ru/javawebinar/basejava/MainCollection.java
@@ -0,0 +1,62 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.*;
+
+/**
+ * Created by Alejandro on 12.07.2017.
+ */
+public class MainCollection {
+
+    private static final String UUID1 = "uuid1";
+    private static final Resume RESUME_1 = new Resume(UUID1);
+    private static final String UUID2 = "uuid2";
+    private static final Resume RESUME_2 = new Resume(UUID2);
+    private static final String UUID3 = "uuid3";
+    private static final Resume RESUME_3 = new Resume(UUID3);
+    private static final String UUID4 = "uuid4";
+    private static final Resume RESUME_4 = new Resume(UUID4);
+    private static final String UUID5 = "uuid5";
+    private static final Resume RESUME_5 = new Resume(UUID5);
+
+    public static void main(String[] args) {
+        Collection<Resume> collection = new ArrayList<>();
+        collection.add(RESUME_1);
+        collection.add(RESUME_2);
+        collection.add(RESUME_3);
+
+        for(Resume r : collection) {
+            System.out.println(r);
+            if(Objects.equals(r.getUuid(), UUID1)) {
+                // collection.remove(r);
+            }
+        }
+
+        Iterator<Resume> iterator = collection.iterator();
+
+        while(iterator.hasNext()) {
+            Resume r = iterator.next();
+            System.out.println(r);
+            if(Objects.equals(r.getUuid(), UUID1)) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println(collection.toString());
+
+        Map<String, Resume> map = new HashMap<>();
+
+        map.put(UUID1, RESUME_1);
+        map.put(UUID2, RESUME_2);
+        map.put(UUID3, RESUME_3);
+
+        for(String uuid : map.keySet()) {
+            System.out.println(map.get(uuid));
+        }
+
+        for(Map.Entry entry : map.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+    }
+}
--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -21,15 +21,24 @@ public abstract class AbstractArrayStorageTest {
     }
 
     private static final String UUID1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID1);
+    private static final Resume RESUME_1;
     private static final String UUID2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID2);
+    private static final Resume RESUME_2;
     private static final String UUID3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID3);
+    private static final Resume RESUME_3;
     private static final String UUID4 = "uuid4";
-    private static final Resume RESUME_4 = new Resume(UUID4);
+    private static final Resume RESUME_4;
     private static final String UUID5 = "uuid5";
-    private static final Resume RESUME_5 = new Resume(UUID5);
+    private static final Resume RESUME_5;
+
+    static {
+        RESUME_1 = new Resume(UUID1);
+        RESUME_2 = new Resume(UUID2);
+        RESUME_3 = new Resume(UUID3);
+        RESUME_4 = new Resume(UUID4);
+        RESUME_5 = new Resume(UUID5);
+
+    }
 
     @Before
     public void setUp() throws Exception {
